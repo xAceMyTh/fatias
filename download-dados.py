60a9ad824d6b886741dc601f68b76bec1597e095
Augusto Herrmann <augusto.herrmann@gmail.com>
2016-03-05 15:16:02 -0300

    leitura do dataset

--- download-dados.py
+++ download-dados.py
@@ -1,3 +1,5 @@
 import ckanapi
 
 dadosgovbr = ckanapi.RemoteCKAN('http://dados.gov.br', user_agent='fatias/1.0 (+http://github.com/augusto-herrmann/fatias)')
+
+dataset_filiados = dadosgovbr.action.package_show(id='filiados-partidos-politicos')
