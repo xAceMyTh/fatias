2f2d2c1e8d011b9efd54c1464ae980fe71888c8a
Augusto Herrmann <augusto.herrmann@gmail.com>
2016-03-05 17:06:44 -0300

    descompacta os arquivos ao baixar

--- download-dados.py
+++ download-dados.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import io
 import os
 import urlparse
+import zipfile
 import requests
 import ckanapi
 
@@ -17,9 +19,12 @@ for url in urls:
     print u'Baixando a url %s' % url
     response = requests.get(url)
     if response.ok:
-        with open('dados/%s' % filename, 'w') as f:
-            print u'Gravando arquivo %s' % filename
-            f.write(response.content)
+        z = zipfile.ZipFile(io.BytesIO(response.content))
+        # queremos apenas o arquivo csv, e nao queremos o sub judice
+        nome_planilha = [name for name in z.namelist() if os.path.splitext(name)[-1] == '.csv' and not 'sub_jud' in name][0]
+        with z.open(nome_planilha) as p, open('dados/%s' % nome_planilha, 'w') as f:
+            print u'Gravando arquivo %s' % nome_planilha
+            f.write(p.read())
     else:
         print 'ERRO: Código %d ao acessar a url.' % response.status_code
 
